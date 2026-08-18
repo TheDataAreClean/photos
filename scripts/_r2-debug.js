dec8c97a80cc8c1ed0b046d4924a546787b5098e
cube <tools@thedataareclean.com>
2026-08-18 14:47:03 +0530

    Chore: extend temporary R2 debug to test GetObject too

--- scripts/_r2-debug.js
+++ scripts/_r2-debug.js
@@ -1,13 +1,32 @@
 const config = require('../config');
-const { S3Client, ListObjectsV2Command } = require('@aws-sdk/client-s3');
+const { S3Client, ListObjectsV2Command, GetObjectCommand } = require('@aws-sdk/client-s3');
 const r2 = config.r2;
 console.log('bucket:', r2.bucket);
 console.log('endpoint host:', r2.endpointUrl ? new URL(r2.endpointUrl).host : null);
 console.log('configured:', !!(r2.bucket && r2.endpointUrl && r2.accessKeyId && r2.secretAccessKey));
 const client = new S3Client({ region: 'auto', endpoint: r2.endpointUrl, credentials: { accessKeyId: r2.accessKeyId, secretAccessKey: r2.secretAccessKey } });
-client.send(new ListObjectsV2Command({ Bucket: r2.bucket })).then(res => {
-  console.log('object count:', (res.Contents || []).length);
-  console.log('first 5 keys:', (res.Contents || []).slice(0, 5).map(o => o.Key));
-}).catch(err => {
-  console.log('R2 LIST ERROR:', err.name, err.message);
-});
+
+async function main() {
+  let firstKey = null;
+  try {
+    const res = await client.send(new ListObjectsV2Command({ Bucket: r2.bucket }));
+    const contents = res.Contents || [];
+    console.log('object count:', contents.length);
+    console.log('first 5 keys:', contents.slice(0, 5).map(o => o.Key));
+    firstKey = contents[0]?.Key;
+  } catch (err) {
+    console.log('R2 LIST ERROR:', err.name, '|', err.message, '|', JSON.stringify(err.$metadata || {}), '|', err.Code || err.code);
+    return;
+  }
+
+  if (!firstKey) { console.log('no keys to test GetObject with'); return; }
+
+  try {
+    const res = await client.send(new GetObjectCommand({ Bucket: r2.bucket, Key: firstKey }));
+    console.log('GET OK for', firstKey, '- content-length:', res.ContentLength);
+  } catch (err) {
+    console.log('R2 GET ERROR:', err.name, '|', err.message, '|', JSON.stringify(err.$metadata || {}), '|', err.Code || err.code);
+  }
+}
+
+main();
